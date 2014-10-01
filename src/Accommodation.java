063ee5c06fecd4aa821230d290486bc9cd20a2fb
lukasz <lukasz@lenovo>
2014-10-01 21:21:09 +0200

    nowe

---
+++ src/Accommodation.java
@@ -0,0 +1,37 @@
+
+public class Accommodation extends Person {
+	
+	char sector;
+	int nrOfGrave;
+	
+	Person person;
+
+	
+	
+	public char getSector() {
+		return sector;
+	}
+
+	public void setSector(char sector) {
+		this.sector = sector;
+	}
+
+	public int getNrOfGrave() {
+		return nrOfGrave;
+	}
+
+	public void setNrOfGrave(int nrOfGrave) {
+		this.nrOfGrave = nrOfGrave;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+	
+
+}
