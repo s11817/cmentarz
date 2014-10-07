5a04d539e71a74ef5029403d403fdac93d6df90b
lukasz <lukasz@lenovo>
2014-10-07 20:38:00 +0200

    Doszły małe testy 07,10,2014

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import javax.swing.JOptionPane;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Start");
+		String name = JOptionPane.showInputDialog("Podaj swoje imieee");
+		JOptionPane.showMessageDialog(null, "Dziêki " + name);
+		System.out.println("name to" + name);
+		//int liczba = Integer.parseInt(name);
 		System.out.println("i mala zmianka");
 		System.out.println("i jeszcze raz w drug¹strone");
 		
