941aedef7f5ba00078df1606d1804daecd196e48
s11817 <s11817@pjwstk.edu.pl>
2014-10-11 13:04:42 +0200

    zmiana 1 na pj

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name = JOptionPane.showInputDialog("Podaj swoje imieee");
-		JOptionPane.showMessageDialog(null, "Dziêki " + name);
+		JOptionPane.showMessageDialog(null, "Dziï¿½ki " + name);
 		System.out.println("name to" + name);
 		//int liczba = Integer.parseInt(name);
-		System.out.println("i mala zmianka");
-		System.out.println("i jeszcze raz w drug¹strone");
+		System.out.println("i mala zmiankasssss");
+		System.out.println("i jeszcze raz w drugï¿½strone");
 		
 	}
 
